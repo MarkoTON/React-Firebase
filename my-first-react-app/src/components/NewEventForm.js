2c3936362dcdb7640da70ea5ccde4755a8951ec8
Marko_TON <markotoncanovic@gmail.com>
2022-09-02 21:31:18 +0200

    reorder JSX

--- my-first-react-app/src/components/NewEventForm.js
+++ my-first-react-app/src/components/NewEventForm.js
@@ -28,21 +28,11 @@ export default function NewEventForm({ addEvent }) {
     <form className="new-event-form" onSubmit={handleSubmit}>
       <label>
         <span>Event Title:</span>
-        <input
-          type="text"
-          onChange={(e) => setTitle(e.target.value)}
-          value={title}
-          required
-        />
+        <input type="text" onChange={(e) => setTitle(e.target.value)} value={title} required />
       </label>
       <label>
         <span>Event Date:</span>
-        <input
-          type="date"
-          onChange={(e) => setDate(e.target.value)}
-          value={date}
-          required
-        />
+        <input type="date" onChange={(e) => setDate(e.target.value)} value={date} required />
       </label>
       <label>
         <span>Event Location:</span>
