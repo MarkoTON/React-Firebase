785a3741ef36456662a7af080591589d1e77a939
Marko_TON <markotoncanovic@gmail.com>
2022-09-02 20:47:03 +0200

    reorganize inline style

--- my-first-react-app/src/components/Modal.js
+++ my-first-react-app/src/components/Modal.js
@@ -2,20 +2,18 @@ import ReactDOM from 'react-dom'
 import './Modal.css'
 
 export default function Modal({ children, handleClose, isSalesModal }) {
+
+  const inlineStyle = { border: "4px solid", borderColor: isSalesModal ? "#ff4500" : "#555", textAlign: "center" }
+
   return ReactDOM.createPortal((
     <div className="modal-backdrop">
-      <div className="modal" style={{ 
-        border: "4px solid", 
-        borderColor: isSalesModal ? "#ff4500" : "#555",
-        textAlign: "center"
-      }}>
+      <div className="modal" style={inlineStyle}>
+
         {children}
-        <button 
-          onClick={handleClose} 
-          className={isSalesModal ? "sales-btn": "" }
-        >
-          Close
-        </button>
+        <hr />
+        <i>Button is comming from modal. And Form from NewEventForm that is insert over children in App.js!!!</i>
+        <br />
+        <button onClick={handleClose} className={isSalesModal ? "sales-btn": "" }>Close</button>
       </div>
     </div>
   ), document.body)
