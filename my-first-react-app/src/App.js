733f4fa2aeafa27e6441f84bd6f3c647454c9ba6
markoton <markotoncanovic@gmail.com>
2023-07-08 21:25:31 +0200

    check func and events

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -14,11 +14,25 @@ function App() {
     console.log(name)
     console.log('---4---');
   }
+  
+  const handleClickTwo = () => {
+    console.log('---1---');
+    console.log(name)
+    console.log('---2---');
+    setName('luigi')
+    console.log(setName('luigi'));
+    console.log('---3---');
+    console.log(name)
+    console.log('---4---');
+  }
 
   return (
     <div className="App">
       <h1>My name is {name}</h1>
-      <button onClick={handleClick}>Change name</button>
+      <h1>Postoji razlika u okidanju kada se stavi 'onClick= handleClick' u odnosu na ' onClick=(e) = handleClickTwo()'</h1>
+      <button onClick={handleClick}>Change name</button> 
+      <br />
+      <button onClick={(e) => handleClickTwo()}>Change name Two</button>
     </div>
   );
 }
